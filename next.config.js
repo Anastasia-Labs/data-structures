cc506a801afa1e11304844468dfffd0cf4e16e56
hade <liveithade@gmail.com>
2024-04-09 00:03:45 +0700

    docs: init nextra docs

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  defaultShowCopyCode: true,
+  readingTime: true,
+})
+ 
+module.exports = {
+  ...withNextra({
+    images: { unoptimized: true },
+  }),
+  output: 'export',
+  basePath: "/data-structures",
+}
